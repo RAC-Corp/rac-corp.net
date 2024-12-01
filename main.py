b2aa800c508ed6db7107cd35b48a5550ff051ce4
sledgehammer <x.pham77@yahoo.com>
2024-11-30 19:26:23 -0800

    Create __init__.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse, Response, RedirectResponse
+from fastapi.requests import Request
+
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler) # type: ignore
 
 
 @app.get('/')
-async def root():
+@limiter.limit('10/minute')
+async def root(request: Request):
     return JSONResponse({'content': 'hello world'})
